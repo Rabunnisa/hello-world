86e43d5c4c36c807e1714c802b649e61572235d4
Rabunnisa Zabiulla <79481121+Rabunnisa@users.noreply.github.com>
2021-02-27 20:51:06 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,3 +1,6 @@
 System.out.println("hello every one ");
 System.out.println("It was a sunny day today  ");
 System.out.println("kids enjoyed a lot ");
+System.out.println("We are palnning to do a party tomorrow ");
+System.out.println("Zara loved playing out today  ");
+System.out.println("Rayyan was out with his friends  ");
